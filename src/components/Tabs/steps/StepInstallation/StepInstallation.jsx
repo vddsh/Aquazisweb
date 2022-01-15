06b7da28c0c5715e592a502f188990e35b206def
vddsh <myemeil123@gmail.com>
2022-01-15 13:24:10 +0200

    instal interface

--- src/components/Tabs/steps/StepInstallation/StepInstallation.jsx
+++ src/components/Tabs/steps/StepInstallation/StepInstallation.jsx
@@ -8,91 +8,93 @@ function StepInstallation() {
     dispatch,
   } = useContactFormState();
   return (
-    <div className="installation__wrap">
-      <div className="installation__row">
-        <div className="installation__box">
-          <label className={installation === "Nerjav" ? "checked" : ""}>
-            <input
-              type="radio"
-              id="1"
-              name="installation"
-              value="Nerjav"
-              checked={installation === "Nerjav"}
-              onChange={(e) =>
-                dispatch({
-                  type: "INSTALLATION_CHANGE",
-                  payload: e.target.value,
-                })
-              }
-            />
-            <div>
-              <img src="./img/profile-1.jpg" />
-              <h6>Ножки из нержавеющей стали</h6>
-            </div>
-          </label>
-        </div>
-        <div className="installation__box">
-          <label className={installation === "Podves" ? "checked" : ""}>
-            <input
-              type="radio"
-              id="2"
-              name="installation"
-              value="Podves"
-              checked={installation === "Podves"}
-              onChange={(e) =>
-                dispatch({
-                  type: "INSTALLATION_CHANGE",
-                  payload: e.target.value,
-                })
-              }
-            />
-            <div>
-              <img src="./img/profile-2.jpg" />
-              <h6>Подвесной крепеж</h6>
-            </div>
-          </label>
-        </div>
-        <div className="installation__box">
-          <label className={installation === "Acril1" ? "checked" : ""}>
-            <input
-              type="radio"
-              id="3"
-              name="installation"
-              value="Acril1"
-              checked={installation === "Acril1"}
-              onChange={(e) =>
-                dispatch({
-                  type: "INSTALLATION_CHANGE",
-                  payload: e.target.value,
-                })
-              }
-            />
-            <div>
-              <img src="./img/profile-3.jpg" />
-              <h6>Акриловые ножки (вариант 1)</h6>
-            </div>
-          </label>
-        </div>
-        <div className="installation__box">
-          <label className={installation === "Acril2" ? "checked" : ""}>
-            <input
-              type="radio"
-              id="4"
-              name="installation"
-              value="Acril2"
-              checked={installation === "Acril2"}
-              onChange={(e) =>
-                dispatch({
-                  type: "INSTALLATION_CHANGE",
-                  payload: e.target.value,
-                })
-              }
-            />
-            <div>
-              <img src="./img/profile-4.jpg" />
-              <h6>Акриловые ножки (вариант 2)</h6>
-            </div>
-          </label>
+    <div className="categories__row">
+      <div className="tabcontent active">
+        <div className="tab__row">
+          <div className="tab__box">
+            <label className={installation === "Nerjav" ? "checked" : ""}>
+              <input
+                type="radio"
+                id="1"
+                name="installation"
+                value="Nerjav"
+                checked={installation === "Nerjav"}
+                onChange={(e) =>
+                  dispatch({
+                    type: "INSTALLATION_CHANGE",
+                    payload: e.target.value,
+                  })
+                }
+              />
+              <div className="categories__border">
+                <img src="./img/profile-1.jpg" />
+                <h6>Ножки из нержавеющей стали</h6>
+              </div>
+            </label>
+          </div>
+          <div className="tab__box">
+            <label className={installation === "Podves" ? "checked" : ""}>
+              <input
+                type="radio"
+                id="2"
+                name="installation"
+                value="Podves"
+                checked={installation === "Podves"}
+                onChange={(e) =>
+                  dispatch({
+                    type: "INSTALLATION_CHANGE",
+                    payload: e.target.value,
+                  })
+                }
+              />
+              <div className="categories__border">
+                <img src="./img/profile-2.jpg" />
+                <h6>Подвесной крепеж</h6>
+              </div>
+            </label>
+          </div>
+          <div className="tab__box">
+            <label className={installation === "Acril1" ? "checked" : ""}>
+              <input
+                type="radio"
+                id="3"
+                name="installation"
+                value="Acril1"
+                checked={installation === "Acril1"}
+                onChange={(e) =>
+                  dispatch({
+                    type: "INSTALLATION_CHANGE",
+                    payload: e.target.value,
+                  })
+                }
+              />
+              <div className="categories__border">
+                <img src="./img/profile-3.jpg" />
+                <h6>Акриловые ножки (вариант 1)</h6>
+              </div>
+            </label>
+          </div>
+          <div className="tab__box">
+            <label className={installation === "Acril2" ? "checked" : ""}>
+              <input
+                type="radio"
+                id="4"
+                name="installation"
+                value="Acril2"
+                checked={installation === "Acril2"}
+                onChange={(e) =>
+                  dispatch({
+                    type: "INSTALLATION_CHANGE",
+                    payload: e.target.value,
+                  })
+                }
+              />
+              <div className="categories__border">
+                <img src="./img/profile-4.jpg" />
+                <h6>Акриловые ножки (вариант 2)</h6>
+              </div>
+            </label>
+          </div>
         </div>
       </div>
     </div>
